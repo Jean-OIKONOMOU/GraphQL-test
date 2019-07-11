c8fad64f9a77bb077f8e51f7da372c4ccebe742f
Jean <jean.oikonomou@gmail.com>
2019-07-11 12:07:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'); // 1.
+const graphqlHTTP = require('express-graphql'); // 4. création du serveur express qui va executer graphql api
+const schema = require('./schema/schema') // 6. une fois notre schema defini (cf. schema.js), on l'importe 
+
+const app = express(); // 2.
+
+app.use('/graphql', graphqlHTTP({ // 5. fonction servant de noeud central/middleware qui va envoyer toutes les requetes graphql en un seul endroit
+    // placer schema ici ulterieurement
+    schema,
+    graphiql: true
+}));
+
+app.listen(1337, () => { // 3. une fois installé nodemon, va "ecouter les changements" in browser: http://localhost:4000/graphql 
+    console.log('now listening for request on port 1337 bruh...');
+})
